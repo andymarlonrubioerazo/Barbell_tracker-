d54a459b9fdb012b99fdd32b66af533b7596ca94
AndyRubioErazo <60636978+andymarlonrubioerazo@users.noreply.github.com>
2025-09-12 12:48:05 -0500

    Update Tracker_app.py

--- Tracker_app.py
+++ Tracker_app.py
@@ -274,7 +274,6 @@ class WeightliftingApp:
             if frames/fps>60.:
                 st.warning(f'No process {video.name}. The video is too long.\n Upload a video shorter than 60 seconds.')
                 os.remove(video_name)
-                st.write('boorando video')
                 continue
 
             # Process video
