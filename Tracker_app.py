94448ee8b583dad28ca795508c85e2575ded407e
andymarlonrubioerazo <andyrubio-16@hotmail.com>
2025-09-12 17:37:56 -0500

    hghg

--- Tracker_app.py
+++ Tracker_app.py
@@ -141,8 +141,7 @@ class VideoProcessor:
                 # Verificar si todas las clases existen
 
         try:
-
-            st.write(os.path.exists(path_video))
+            st.write(os.path.exists(model_selected), model_classes)
 
             results = model(
                 source=path_video,
